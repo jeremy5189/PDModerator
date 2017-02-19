9581eb568f7d42248b6664834aeec3df60809046
Jeremy Yen <jeremy5189@gmail.com>
2017-02-19 18:00:29 +0800

    Fix www/bin again

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+// websocket test
 router.ws('/echo', function(ws, req) {
     ws.on('message', function(msg) {
         console.log(msg);
